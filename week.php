d6e3ebc98abf9d57d44427c3ff7d979199521e0c
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-03-09 22:21:49 +0100

    commit

--- week.php
+++ week.php
@@ -144,8 +144,7 @@ $nextavailable = "";
 if (!$default_bookings_open) {
   $next_start_time = get_next_available_start_time($room,$year.$month.$day);
   if ($next_start_time) {
-    $nextavailable = "
-            &nbsp;-&nbsp;<a href=\"week.php?year=" . date("Y",$next_start_time). 
+    $nextavailable = "&nbsp;-&nbsp;<a href=\"week.php?year=" . date("Y",$next_start_time). 
             "&amp;month=". date("Y",$next_start_time) . 
             "&amp;day=" .date("Y",$next_start_time) . 
             "&amp;area=$area&amp;room=$room\">"
