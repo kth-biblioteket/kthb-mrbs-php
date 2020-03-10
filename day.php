f95499dd387a3db70fb380c0b8f46e395a5bf3b1
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-03-10 09:18:50 +0100

    commit

--- day.php
+++ day.php
@@ -77,12 +77,12 @@ if ($extra_info != "") {
 //KTH Modal box
 //191003
 get_area_settings($area);
-echo "<div style=\"padding-bottom:10px\">";
+echo "<div style=\"padding-bottom:10px\">$room";
 print_modal($area,$lang,$area_map,$area_map_image);
 //200309 visa länk till nästa lediga bokning om bokningar är stängda som default
 $nextavailable = "";
 if (!$default_bookings_open) {
-  $next_start_time = get_next_available_start_time($room,date('Y-m-d'));
+  $next_start_time = get_next_available_start_time($room, date('Y-m-d'));
   if ($next_start_time) {
     $nextavailable = "
     <div style=\"text-align:right\">
