093328e1f88963b7344ce298672de204a83cc5b1
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-03-10 10:23:16 +0100

    commit

--- week.php
+++ week.php
@@ -86,6 +86,7 @@ if ($extra_info != "") {
 
 get_area_settings($area);
 echo "<div style=\"padding-bottom:10px;overflow:auto\">";
+echo $year . $month . $day;
 print_modal($area,$lang,$area_map,$area_map_image);
 //200309 visa länk till nästa lediga bokning om bokningar är stängda som default
 $nextavailable = "";
