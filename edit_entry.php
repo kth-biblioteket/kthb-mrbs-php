9b5d94d024c912d6ab1098d55dd216e6c69182d7
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-03-27 23:05:02 +0100

    'commit'

--- edit_entry.php
+++ edit_entry.php
@@ -762,8 +762,8 @@ function create_field_entry_custom_field($field, $key, $is_admin, $user, $disabl
                   'disabled'    => $disabled,
                   'options'     => array(),
                   'force_assoc' => TRUE,  // in case the type keys happen to be digits
-                  'value'       => isset($custom_fields[$key]) ? $custom_fields[$key] : 'disabled');
-                  'mandatory'  => isset($is_mandatory_field["entry.$key"]) && $is_mandatory_field["entry.$key"]);
+                  'value'       => isset($custom_fields[$key]) ? $custom_fields[$key] : 'disabled',
+                  'mandatory'   => isset($is_mandatory_field["entry.$key"]) && $is_mandatory_field["entry.$key"]);
 
         $places = array();
         $sql = "SELECT kth_places.id, kth_places.name, kth_places.name_en
