1bc436b11e5dea877f29fd7517efdd2482db3aef
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-03-29 18:52:23 +0200

    'commit'

--- week.php
+++ week.php
@@ -103,11 +103,16 @@ print_modal($area,$lang,$area_map,$area_map_image);
 //200309 visa länk till nästa lediga bokning om bokningar är stängda som default
 //TODO hämta aktuell veckas sista slot och kolla om det finns lediga tider efter den
 $currentviewdate = mktime(0, 0, 0, $month, $day, $year);
-$week_end = date('Y-m-d', strtotime('this week next sunday', $currentviewdate));
+if (date('N',$currentviewdate == 7)) {
+  $week_end = date('Y-m-d', strtotime('this week sunday', $currentviewdate));
+} else {
+  $week_end = date('Y-m-d', strtotime('this week next sunday', $currentviewdate));
+}
+
 $nextavailable = "";
 if (!$default_bookings_open) {
   //$next_start_time = get_next_available_start_time_room($room,$ty.$tm.$td);
-  $next_start_time = get_next_available_start_time_room($room,$week_end);
+  $next_start_time = get_next_available_start_time_room($room, $week_end);
   //$next_start_time = get_next_available_start_time($room,$year ."-". $month ."-". $day);
   if ($next_start_time) {
     $nextavailable = "
