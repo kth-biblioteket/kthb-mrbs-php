8a1c0623168a1b1703003d719415d7f81ba96c9b
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-10-23 15:38:35 +0200

    add new field sql

---
+++ config/sql/addnewfield.sql
@@ -0,0 +1,37 @@
+/* Exempel på att lägga till ett nytt fält */
+
+/* KTH Campus för Handledning */
+
+ALTER TABLE `mrbs_entry` ADD `my_campus` INT NULL DEFAULT NULL AFTER `campus`;
+
+ALTER TABLE `mrbs_repeat` ADD `my_campus` INT NULL DEFAULT NULL AFTER `campus`;
+
+ALTER TABLE `mrbs_entry_delete` ADD `my_campus` INT NULL DEFAULT NULL AFTER `campus`;
+
+CREATE TABLE IF NOT EXISTS `kth_campus` (
+  `id` int(11) NOT NULL,
+  `disabled` tinyint(4) NOT NULL DEFAULT '0',
+  `sort_key` varchar(25) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `name_en` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `kth_campus`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `kth_campus`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `kth_campus` (`disabled`, `sort_key`, `name`, `name_en`) VALUES
+(0, '1', 'Campus KTH', 'Campus KTH'),
+(0, '2', 'Campus Kista', 'Campus Kista'),
+(0, '3', 'Campus Södertälje', 'Campus Södertälje'),
+(0, '4', 'Campus Flemingsberg', 'Campus Flemingsberg'),
+(0, '5', 'Campus Solna', 'Campus Solna'),
+(0, '0', '1none', '1none');
+
+/*Kör bara för den Applikation som ska ha med fältet i bokningen*/
+/*
+INSERT INTO `kth_entry_custom_fields` (`area_id`, `entry_field_name`, `type`, `sort_order`, `visible`, `enabled`, `mandatory`) VALUES
+(1, 'my_campus', 'custom', 411, 1, 1, 1);
+*/
