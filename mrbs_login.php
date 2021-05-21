f33b75f9254ed9000d3b97aa97ffda2a8d133c41
Thomas Lind <thomas.lind@tlcgolfit.se>
2021-05-21 09:23:02 +0200

    KTH new login OpenID

--- mrbs_login.php
+++ mrbs_login.php
@@ -24,9 +24,11 @@ if(isset($_GET['sessionname'])) {
 	session_name($sessionname);
 }
 	
+
 $_SERVER['HTTP_X_FORWARDED_HOST'] = $_SERVER['HTTP_HOST'];
 $_SERVER['REQUEST_URI']=html_entity_decode("/" . $app_name . "/mrbs_login.php?sessionname=" . $sessionname . "&formlanguage=" . $formlanguage . "&returl=" . $returl);
 
+/*
 require_once $_SERVER['DOCUMENT_ROOT'] . '/CAS/CAS.php';
 
 // Uncomment to enable debugging
@@ -46,4 +48,42 @@ if($casUser) {
 	$returl = str_replace('ampersand','&',$returl);
 	header("location: " . $returl);
 }
+*/
+
+
+//210519 OpenID Connect framework(myits)
+require_once($_SERVER['DOCUMENT_ROOT'] .  '/myits/vendor/autoload.php');
+
+use Its\Sso\OpenIDConnectClient;
+use Its\Sso\OpenIDConnectClientException;
+
+try {
+    $oidc = new OpenIDConnectClient(
+		$kth_auth_endpoint,
+		$kth_client_id,
+		$kth_client_secret
+	);
+ 
+    $oidc->setRedirectURL("https://" . $_SERVER['HTTP_HOST'] . "/" . $app_name); // must be the same as you registered
+    $oidc->addScope('openid email'); //must be the same as you registered
+    
+    // remove this if in production mode
+    $oidc->setVerifyHost(false);
+    $oidc->setVerifyPeer(false);
+	
+    $oidc->authenticate(); //call the main function of SSO login
+    $_SESSION['id_token'] = $oidc->getIdToken(); // must be save for check session dan logout proccess
+	$kthUser = $oidc->requestUserInfo(); // this will return user information from SSO database
+	if($kthUser) {
+		if (session_status() == PHP_SESSION_NONE) {
+			session_start();
+		}
+		$_SESSION['kth_id'] = $kthUser->email;
+		$userid = $_SESSION['kth_id']  ;
+	}
+    
+} catch (OpenIDConnectClientException $e) {
+    echo $e->getMessage();
+}
+
 ?>
